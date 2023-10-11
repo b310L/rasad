4b178f4fb7d9b193150915446b99715804aa4352
b310l <mmhosein.hp@gmail.com>
2023-10-11 12:33:00 +0330

    Add tablet layout and bottom navigation in it

---
+++ src/app/(justForTablet)/tablet/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
+import React, { useState } from 'react'
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+const page = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+  <>tablet screen additional
+  </>
+  )
+}
+
+export default page
